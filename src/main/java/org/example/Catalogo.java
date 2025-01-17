7988821fb251fb84e9965f6766e845462d338ee8
annunziatadr99 <anudar99@hotmail.it>
2025-01-17 17:07:11 +0100

    Continuare creando classe Riviste

--- src/main/java/org/example/Catalogo.java
+++ src/main/java/org/example/Catalogo.java
@@ -1,4 +1,36 @@
 package org.example;
 
-public class Catalogo {
+public abstract class Catalogo{
+    protected String isbn;
+    protected String titolo;
+    protected int annoDiPubblicazione;
+    protected int numeroDiPagine;
+
+
+    public Catalogo (String isbn, String titolo, int yearOfPublication, int numberOfPages) {
+        this.isbn = isbn;
+        this.titolo = titolo;
+        this.annoDiPubblicazione = annoDiPubblicazione;
+        this.numeroDiPagine = numeroDiPagine;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public int getAnnoDiPubblicazione() {
+        return annoDiPubblicazione;
+    }
+
+    public int getNumeroDiPagine() {
+        return numeroDiPagine;
+    }
+
+
+    public String toString() {
+        return "ISBN: " + isbn + ", Titolo: " + titolo + ", Anno: " + annoDiPubblicazione + ", Pagine: " + numeroDiPagine;
+    }
 }
--- src/main/java/org/example/Libri.java
+++ src/main/java/org/example/Libri.java
@@ -1,4 +1,24 @@
 package org.example;
 
-public class Libri {
+public class Libri extends Catalogo{
+    private String autore;
+    private String genere;
+
+    public Libri(String isbn, String titolo, int annoDiPubblicazione, int numeroDiPagine) {
+        super(isbn, titolo, annoDiPubblicazione, numeroDiPagine);
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public String toString() {
+        return super.toString() + ", Autore: " + autore + ", Genere: " + genere;
+    }
 }
+
+
